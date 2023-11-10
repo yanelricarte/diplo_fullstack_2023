b8b2709ba44c5c5e057399be489218af4873f291
Naranjas <yanelricarte@gmail.com>
2023-11-09 21:07:09 -0300

    Clase 20: uso de props

---
+++ react-vite/proyecto-react-vite/src/componentes/Casa.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import imagenes from './imagenes';
+
+
+
+function Casa(props){
+    return(
+        <section className="seccion">
+        <h2> {props.nombre}</h2>
+        <p> {props.valores}</p>
+        <p> {props.animal}</p>
+        <p> {props.director}</p>
+        <p> {props.ubicacion}</p>
+        <img src={imagenes[props.imagen]} alt={props.nombre}/>
+
+
+        </section>
+    )
+}
+
+Casa.propTypes = {
+    nombre: PropTypes.string.isRequired,
+    valores: PropTypes.string,
+    animal: PropTypes.string,
+    color: PropTypes.string,
+    director: PropTypes.string,
+    ubicacion: PropTypes.string,
+    imagen: PropTypes.string,
+
+};
+
+
+export default Casa;
